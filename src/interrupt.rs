1bf256b9da22eb16fe30934b0c1ffcd29b96e91e
ytoku <ytoku42@gmail.com>
2023-10-15 08:04:00 +0900

    Remove fatuous OnceLock and Arc

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,17 +1,14 @@
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
-use std::sync::OnceLock;
 
-static INTERRUPTED: OnceLock<Arc<AtomicBool>> = OnceLock::new();
+static INTERRUPTED: AtomicBool = AtomicBool::new(false);
 
 pub fn register_ctrlc() {
-    let setter = Arc::clone(INTERRUPTED.get_or_init(|| Arc::new(AtomicBool::new(false))));
     ctrlc::set_handler(move || {
-        setter.store(true, Ordering::SeqCst);
+        INTERRUPTED.store(true, Ordering::SeqCst);
     })
     .expect("Error setting Ctrl-C handler");
 }
 
 pub fn interrupted() -> bool {
-    INTERRUPTED.get().unwrap().load(Ordering::SeqCst)
+    INTERRUPTED.load(Ordering::SeqCst)
 }
