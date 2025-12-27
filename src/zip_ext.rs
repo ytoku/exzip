95d0624583d05885be44b72b490d41dcf33aaeda
ytoku <ytoku42@gmail.com>
2025-12-28 03:31:10 +0900

    Upgrade zip crate from 0.6.x to 7.0.0

--- src/zip_ext.rs
+++ src/zip_ext.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::path::PathBuf;
 
 use chrono::{DateTime, Local, LocalResult, TimeZone};
@@ -5,14 +6,14 @@ use zip::read::ZipFile;
 
 use crate::encoding::ZipEncoding;
 
-pub trait ZipFileExt<'a> {
+pub trait ZipFileExt<'a, R: Read> {
     fn decoded_name_lossy(&self, encoding: ZipEncoding) -> PathBuf;
     fn is_utf8(&self) -> bool;
 
     fn last_modified_chrono(&self) -> LocalResult<DateTime<Local>>;
 }
 
-impl<'a> ZipFileExt<'a> for ZipFile<'a> {
+impl<'a, R: Read> ZipFileExt<'a, R> for ZipFile<'a, R> {
     fn decoded_name_lossy(&self, encoding: ZipEncoding) -> PathBuf {
         if self.is_utf8() {
             return PathBuf::from(self.name());
@@ -34,13 +35,15 @@ impl<'a> ZipFileExt<'a> for ZipFile<'a> {
         // the flag but we can get the offset of the central header by
         // ZipFile::central_header_start.  However ZipFile cannot
         // access to the zip file reader.
-        // https://github.com/zip-rs/zip/blob/3e88fe66c941d411cff5cf49778ba08c2ed93801/src/read.rs#L671
+        // https://github.com/zip-rs/zip2/blob/v7.0.0/src/read.rs#L1375
         let (utf8_cow, _encoding, malformed) = encoding_rs::UTF_8.decode(self.name_raw());
         !malformed && self.name() == utf8_cow
     }
 
     fn last_modified_chrono(&self) -> LocalResult<DateTime<Local>> {
-        let zip_dt = self.last_modified();
+        let Some(zip_dt) = self.last_modified() else {
+            return LocalResult::None;
+        };
         Local.with_ymd_and_hms(
             zip_dt.year().into(),
             zip_dt.month().into(),
